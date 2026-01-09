af78d0edd376cc5977f4978fe816cb50a7988486
JpSchimidt <jpschimidtfuentes@gmail.com>
2026-01-09 09:21:55 -0300

    exibindo meu primeiro aluno

--- main.go
+++ main.go
@@ -4,7 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func ExibeTodosAlunos(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":    1,
+		"nome":  "João",
+		"idade": 25,
+	})
+}
+
 func main() {
 	r := gin.Default()
+	r.GET("/alunos", ExibeTodosAlunos)
 	r.Run()
 }
